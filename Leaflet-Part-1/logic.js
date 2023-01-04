7d19c543b9a96d1010d3a4d129c03fe6edb098da
jananeesaranraj <jananee.saran@gmail.com>
2023-01-04 17:23:49 -0500

    added the changes

---
+++ Leaflet-Part-1/logic.js
@@ -0,0 +1,132 @@
+// Store our API endpoint as Url.
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(url).then(function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function.
+    createFeatures(data.features);
+});
+
+function getColor(d) {
+    return d > 90 ? 'red' :
+        d > 70 ? '#FF8C00' :
+            d > 50 ? '#FF7F50' :
+                d > 30 ? '#FFD700' :
+                    d > 10 ? '#32cd32' :
+                        '#ADFF2F';
+};
+
+
+function createFeatures(earthquakeData) {
+
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Time: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p>
+        <p>Depth: ${feature.geometry.coordinates[2]}</p>`)
+    }
+
+    function pointToLayer(feature, latlng) {
+        var earthquakeMarkers = {
+            fillOpacity: 0.75,
+            color: "grey",
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            radius: feature.properties.mag * 40000,
+            weight: 0.25
+        }
+        return L.circle(latlng, earthquakeMarkers)
+
+    }
+
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: pointToLayer,
+    });
+
+    // // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+    // console.log(earthquakes)
+
+    // Create the base layers.
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+    // var street = L.tileLayer('https://{s}.tile.opensatellitemap.org/{z}/{x}/{y}.png', {
+    //     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    // });
+
+    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+    var sat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    // Create a baseMaps object.
+    var baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo,
+        "Satellite": sat
+    };
+
+    // Create an overlay object to hold our overlay.
+    var overlays = {
+        Earthquakes: earthquakes
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+    var myMap = L.map("map", {
+        center: [40.7128, -74.0059],
+        zoom: 4,
+        layers: [street, earthquakes]
+    });
+    // Add the layer control to the map.
+    L.control.layers(baseMaps, overlays, {
+        collapsed: false
+    }).addTo(myMap);
+
+    // add legend
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            depth = [-10, 10, 30, 50, 70, 90];
+        // loop through our magnitude intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < depth.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(depth[i] + 1) + '"></i> ' +
+                depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    legend.addTo(myMap);
+}
+
+// function updateLegend(myMap) {
+
+
+
+
+
+
+
+
+
+//     var legend = L.control({ position: 'bottomright' });
+//     legend.onAdd = function (myMap) {
+
+//         var div = L.DomUtil.create('div', 'info legend');
+//         labels = ['90+', '70-90', '50-70', '30-50', '10-30', '-10-10']
+//         for (var i = 0; i < labels.length; i++) {
+//             div.innerHTML +=
+//                 labels.push(
+//                     '<i class="circle" style="background:' + chooseColor(labels[i]) + '"></i> ' +
+//                     (labels[i] ? labels[i] : '+'));
+//         }
+//         div.innerHTML = labels.join('<br>');
+//         return div;
+//     };
+//     legend.addTo(myMap);
+// }
+
---
+++ Leaflet-Part-2/logic.js
@@ -0,0 +1,6 @@
+var platesUrl = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json";
+
+// Perform a GET request to the query URL/
+d3.json(platesUrl).then(function (data) {
+
+})
